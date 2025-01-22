2bc62fe4bfde96becca2e769508f851853a35b89
Talin Roche <talin.roche@gmail.com>
2025-01-22 19:35:31 +1100

    homepage stupid iphone scrolling fixed

--- src/Pages/combinedPage.jsx
+++ src/Pages/combinedPage.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import FloatingObjectsBackground from './floatingObjectsBackground';
 import HomePage from './home';
+import styles from '../Styles/home.module.css'
 
 function CombinedPage() {
   return (
     <>
+    <div className = {styles.mainPage}>
       <FloatingObjectsBackground />
       <HomePage />
+    </div>
     </>
   );
 }

422c094905b2f6ba5b0e36d20f218ff63778bdb0
Talin Roche <talin.roche@gmail.com>
2025-01-24 02:13:19 +1100

    products

--- src/Pages/testingSnipcart.jsx
+++ src/Pages/testingSnipcart.jsx
@@ -15,7 +15,7 @@ const TestingSnipcart = () => {
               <p>{product.description}</p>
               <p>${product.price}</p>
               <p>
-                <button 
+                {/* <button 
                   className="snipcart-add-item"
                   data-item-id={product.id}
                   data-item-image={product.image}
@@ -23,19 +23,18 @@ const TestingSnipcart = () => {
                   data-item-price={product.price}
                 >
                   Add to Cart
-                </button>
+                </button> */}
+
                 <button 
-                  class="snipcart-add-item"
-                  data-item-id="starry-night"
-                  data-item-price="79.99"
-                  data-item-url="/paintings/starry-night"
-                  data-item-description="High-quality replica of The Starry Night by the Dutch post-impressionist painter Vincent van Gogh."
-                  data-item-image="/assets/images/starry-night.jpg"
-                  data-item-name="The Starry Night"
-                  data-item-custom1-name="Frame color"
-                  data-item-custom1-options="Black|Brown|Gold">
-                  
-                </button>
+                className="snipcart-add-item"
+                data-item-id="example-item"
+                data-item-name="Example Product"
+                data-item-price="19.99"
+                data-item-url="https://time-in-lieu-site-git-main-talinrs-projects.vercel.app/snipcart-test"
+                data-item-description="An example product for testing.">
+                Add to Cart
+              </button>
+
               </p>
             </div>
           );

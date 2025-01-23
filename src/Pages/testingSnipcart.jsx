5cc68ac0bc22f02751a859a9e779c273d10af291
Talin Roche <talin.roche@gmail.com>
2025-01-24 02:00:16 +1100

    products

--- src/Pages/testingSnipcart.jsx
+++ src/Pages/testingSnipcart.jsx
@@ -24,6 +24,18 @@ const TestingSnipcart = () => {
                 >
                   Add to Cart
                 </button>
+                <button 
+                  class="snipcart-add-item"
+                  data-item-id="starry-night"
+                  data-item-price="79.99"
+                  data-item-url="/paintings/starry-night"
+                  data-item-description="High-quality replica of The Starry Night by the Dutch post-impressionist painter Vincent van Gogh."
+                  data-item-image="/assets/images/starry-night.jpg"
+                  data-item-name="The Starry Night"
+                  data-item-custom1-name="Frame color"
+                  data-item-custom1-options="Black|Brown|Gold">
+                  
+                </button>
               </p>
             </div>
           );
